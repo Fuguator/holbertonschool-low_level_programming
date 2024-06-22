d7cdcbb59ccaeeb8a561ea6fee43effd222546b4
Your Name <you@example.com>
2024-06-22 19:51:33 +0400

    commit

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -13,7 +13,7 @@ int main(void)
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		putchar (alphabet);
 	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
-                putchar (alphabet);
+		putchar (alphabet);
 
 	putchar ('\n');
 	return (0);
