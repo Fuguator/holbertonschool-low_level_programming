05d5999e954673351a79e02d48732fcf331c127a
Your Name <you@example.com>
2024-06-22 09:15:04 +0000

    commit

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -9,8 +9,7 @@
  */
 int main(void)
 {
-	char *message = "and that piece of art is useful
-		\" - Dora Korpar, 2015-10-19\n";
+	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(2, message, 59);
 	return (1);
 }
