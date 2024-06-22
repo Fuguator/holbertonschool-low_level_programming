7b7d9cf45a63fd6e2b07848fc499b0cb7c83d6e9
Your Name <you@example.com>
2024-06-22 19:56:56 +0400

    commit

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet, except e and q
+ * return: returns 0
+ */
+
+int main(void)
+{
+	char a; //alphabet shortcut
+	
+	for (a = 'a'; a <= 'z'; a++)
+		if (a != e || a != q)
+			putchar(a);
+
+	putchar('\n');
+	return (0);
+}
