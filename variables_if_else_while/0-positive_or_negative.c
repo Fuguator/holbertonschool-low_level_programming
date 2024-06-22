7525032b21f34a8c87b356d38124828dbaee0659
Your Name <you@example.com>
2024-06-22 19:03:14 +0400

    commit

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -4,19 +4,19 @@
 
 /**
  * main - generates random number 
- * Return: always 0
+ *Return: always 0
  */
 
 int main(void)
 {
 	int n;
 
-	srand(time(0));
+	srand (time(0));
 	n = rand() - RAND_MAX / 2;
 	
-	if(n > 0)
+	if (n > 0)
 		printf("%d is positive\n", n);
-	else if(n < 0)
+	else if (n < 0)
 		printf("%d is negative\n", n);
 	else
 		printf("%d is zero\n", n);
