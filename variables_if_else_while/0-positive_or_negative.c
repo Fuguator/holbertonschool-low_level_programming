d4de366c5b03b61e16cad90d73c071a2eec2a32a
Your Name <you@example.com>
2024-06-22 19:00:04 +0400

    commit

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -15,11 +15,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	
 	if(n > 0)
-		printf("is positive\n");
+		printf("is positive");
 	else if(n < 0)
-		printf("is negative\n");
+		printf("is negative");
 	else
-		printf("is zero\n");
+		printf("is zero");
 
 	return (0);
 }
