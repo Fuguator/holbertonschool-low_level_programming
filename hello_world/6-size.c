c346c8dc7b30cc5abeb615e867927fdfab2cb856
Your Name <you@example.com>
2024-06-22 08:12:44 +0000

    commit

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -14,11 +14,10 @@ int main(void)
 	char d;
 	long long int e;
 
-
-	printf("size of int:\n", (unsigned long)sizeof(a));
-	printf("size of long int:\n", (unsigned long)sizeof(b));
-	printf("size of float:\n", (unsigned long)sizeof(c));
-	printf("size of long long int:\n", (unsigned long)sizeof(e));
-	printf("size of char:\n", (unsigned long)sizeof(d));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(e));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(c));
 	return (0);
 }
