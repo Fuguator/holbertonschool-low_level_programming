1ddee4093b6d7ff0883ac227716e0179fca2a55b
Your Name <you@example.com>
2024-06-22 20:34:55 +0400

    commit

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints alphabete in reverse
+ * Return: returns 0
+ */
+int main(void)
+{
+	char alph;
+
+	for (alpt = 'z'; alph < 'a'; alph--)
+		putchar(alph);
+	putchar('\n');
+	return (0);
+}
