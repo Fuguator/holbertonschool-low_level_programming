af6ad5424f821d5d79d84ed831a37d5563b5eff5
Your Name <you@example.com>
2024-06-22 08:09:22 +0000

    commit

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -12,10 +12,13 @@ int main(void)
 	long int b;
 	float c;
 	char d;
+	long long int e;
+
 
 	printf("size of int:\n", (unsigned long)sizeof(a));
 	printf("size of long int:\n", (unsigned long)sizeof(b));
 	printf("size of float:\n", (unsigned long)sizeof(c));
+	printf("size of long long int:\n", (unsigned long)sizeof(e));
 	printf("size of char:\n", (unsigned long)sizeof(d));
 	return (0);
 }
