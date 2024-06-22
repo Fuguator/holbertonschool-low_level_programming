bd4dca6989de44546f7c71791488ba9798e8147d
Your Name <you@example.com>
2024-06-22 20:01:31 +0400

    commit

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -2,17 +2,18 @@
 
 /**
  * main - prints alphabet, except e and q
- * return: returns 0
+ * Return: returns 0
  */
 
 int main(void)
 {
 	char a;
-	
+
 	for (a = 'a'; a <= 'z'; a++)
 		if (a != 'e' && a != 'q')
 			putchar(a);
 
 	putchar('\n');
+
 	return (0);
 }
