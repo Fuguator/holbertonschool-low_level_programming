eb129751a21869486c9f80df246b23140a298674
Your Name <you@example.com>
2024-06-23 19:55:20 +0400

    commit

---
+++ functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - func
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long int num1 = 0, num2 = 1, sum, res;
+
+	while (true)
+	{
+		sum = num1 + num2;
+		if (sum <= 4000000)
+			continue;
+		if ((sum % 2) == 0)
+			res += sum;
+
+		num1 = num2;
+		2 = sum;
+	}
+	printf("%lu", res);
+
+	return (0);
+}
