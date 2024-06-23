cd32272e65c11c9b6b0b37456ccf451efe5a6ae5
Your Name <you@example.com>
2024-06-23 18:59:29 +0400

    commit

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - func
+ * Return: 0
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a < 9; a++)
+	{
+		for (b = a + 1; b < 10; b++)
+		{
+			putchar((a % 10) + '0');
+			putchar((b % 10) + '0');
+			if (a == 8 && b == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
