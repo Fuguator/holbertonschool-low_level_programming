b380c0df535050661e27521a37020356b36959eb
Your Name <you@example.com>
2024-06-23 12:16:07 +0400

    commit

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,8 +1,11 @@
 #include "main.h"
 /**
- * print_sign - func
- * @n: variable
- * Return: 1 if positive, 0 if zero, -1 if negative
+ * print_sign - Prints the sign of a number.
+ * @n: The number of which the sign will be printed.
+ *
+ * Return: 1 if the number is greater than zero,
+ *         0 if the number is zero,
+ *         -1 if the number is less than zero.
  */
 int print_sign(int n)
 {
@@ -11,7 +14,7 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if (n > 0)
+	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
