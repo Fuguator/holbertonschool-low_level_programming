365fbc3f47bb8077633f895038f191ee0cab3ed4
Your Name <you@example.com>
2024-06-23 19:36:01 +0400

    commit

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -16,18 +16,26 @@ void print_times_table(int n)
 				_putchar(',');
 				_putchar(' ');
 				res = a * mult;
-				if (res <= 9)
-					_putchar(' ');
-				else if (res <= 99)
-					putchar(' ');
-				if (res > 99)
+				switch (res / 100)
 				{
-					_putchar((res / 100) + '0');
-					_putchar(((res / 10)) % 10 + '0');
-					else if (res > 9 && res <= 99)
-						_putchar((prod / 10) + '0');
-					_putchar((res % 10) + '0');
+					case 0:
+					if (res <= 99)
+						_putchar(' ')
+					if (res <= 9)
+						_putchar(' ');
+					if (res <= 99 && res >= 10)
+						_putchar((res / 10) + '0');
+					break;
+
+					default:
+						_putchar((res / 100) + '0');
+						_putchar(((res / 10)) % 10 + '0');
+						break;
 				}
+				_putchar((res % 10) + '0');
 			}
+			_putchar('\n');
 		}
 }
+
+
