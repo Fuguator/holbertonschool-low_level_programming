db1dbe87e2ad25ac4b060dce54a9a78c303ea8bb
Your Name <you@example.com>
2024-06-23 12:33:46 +0400

    commit

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -10,7 +10,7 @@ void jack_bauer(void)
 		for (m = 0; m < 60; m++)
 		{
 			_putchar((h / 10) + '0');
-			_putchar(( h % 10) + '0');
+			_putchar((h % 10) + '0');
 			_putchar(':');
 			_putchar((m / 10) + '0');
 			_putchar((m % 10) + '0');
