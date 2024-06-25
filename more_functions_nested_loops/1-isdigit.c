5ae7a7db0549d8c00cea6a96d06b3637b2e0e266
Your Name <you@example.com>
2024-06-25 14:52:20 +0400

    commit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isdigit - checks if c is digit
+ * @c: checkable variable
+ * Return: 1 if digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 0)
+		return (1);
+	else
+		return (0);
+}
