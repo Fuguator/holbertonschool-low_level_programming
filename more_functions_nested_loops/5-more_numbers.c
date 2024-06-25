34dd916e53bc8097e61923a68271d201b604a19c
Your Name <you@example.com>
2024-06-25 15:30:17 +0400

    commit

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -8,9 +8,9 @@ void more_numbers(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-		for ( b = 0; b <= 14; b++)
+		for (b = 0; b <= 14; b++)
 		{
-			if ( b >= 10)
+			if (b >= 10)
 				_putchar('1');
 			_putchar(b % 10 + '0');
 		}
