5feca1419e5d152e5d22ca957d69444a98708ae1
Your Name <you@example.com>
2024-06-25 14:56:40 +0400

    commit

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - func
+ * @a: first vatiable
+ * @b: second variable
+ * Return: result of a * b
+ */
+int mul(int a, int b)
+{
+	int res = a * b;
+
+	return (res);
+}
