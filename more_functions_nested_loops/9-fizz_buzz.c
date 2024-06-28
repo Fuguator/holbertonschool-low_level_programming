ccbf872099f3a59e1db1071a553d606463027a9b
Your Name <you@example.com>
2024-06-28 12:33:40 +0400

    commit

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -11,20 +11,20 @@ int main(void)
 	{
 		if ((a % 3 == 0) && (a % 5 == 0))
 			printf("FizzBuzz ");
-		else if ((a % 3) == 0)
-			printf("Fizz ");
 		else if ((a % 5) == 0)
 		{
-			printf("Buzz ");
-
-			if (a != 100)
+			if (a == 100)
+			{
 				printf("Buzz");
+				printf("\n");
+			}
 			else
 				printf("Buzz ");
 		}
+		else if ((a % 3) == 0)
+			printf("Fizz ");
 		else
 			printf("%d ", a);
 	}
-	printf("\n");
 	return (0);
 }
