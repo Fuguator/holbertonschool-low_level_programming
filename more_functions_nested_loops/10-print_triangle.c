6c071c682889baf89447a63ed4446b7fbfaec4d9
Your Name <you@example.com>
2024-06-28 13:33:24 +0400

    commit

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -5,15 +5,22 @@
  */
 void print_triangle(int size)
 {
-	int h, w;
-	
+	int a, b = 0, space;
+
 	if (size > 0)
 	{
-		for (h = 0; h < size; h++)
+		while (y++ < size)
 		{
-			for (w = 1; w <= h; w++)
-				_putchar('#');
-			_putchar('\n');
+			space = size - y - 1;
+			while (x++ < size)
+			{
+				if (space > x)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			x = 0;
+			_putchar ('\n');
 		}
 	}
 	else
