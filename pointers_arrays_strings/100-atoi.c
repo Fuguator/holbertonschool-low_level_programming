d9d85d33dd4f4e039d56f71d7d60ca0dab63d8c9
Your Name <you@example.com>
2024-06-29 18:30:46 +0400

    commit

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -6,38 +6,32 @@
  */
 int _atoi(char *s)
 {
-	int i, d, n, len, f, digit;
-
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
+	int i = 0, a = 0, b = 0, n = 0, len = 0, num = 0;
 
 	while (s[len] != '\0')
 		len++;
 
-	while (i < len && f == 0)
+	while (i < len && b == 0)
 	{
 		if (s[i] == '-')
-			++d;
+			++a;
 
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
+			if (a % 2)
+				num = -num;
+			n = n * 10 + num;
+			b = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
-			f = 0;
+			b = 0;
 		}
+
 		i++;
 	}
 
-	if (f == 0)
+	if (b == 0)
 		return (0);
 
 	return (n);
