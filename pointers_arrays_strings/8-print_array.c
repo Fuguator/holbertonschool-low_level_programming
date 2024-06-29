b57eaa2ece9ea44daedb7852c1c4ca82e3f8d4b0
Your Name <you@example.com>
2024-06-29 17:35:31 +0400

    commit

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,11 +1,20 @@
 #include <stdio>
 #include "main.h"
 /**
+ * print_array - func
+ * @a: array
+ * @n: size oof array
  */
 void print_array(int *a, int n)
 {
 	int i = 0;
 
-	for (i = 0; i < n; a++)
-		printf("%d", a[i]);
+	for (i = 0; i < n; i++)
+	{
+		if (i != 0)
+			printf(", %d", a[i]);
+		else
+			printf("%d", a[i]);
+	}
+	printf("\n");
 }
