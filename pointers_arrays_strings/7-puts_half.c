c1dcbdb16f8b5f5e2b1cb4668057567626cfb9fd
Your Name <you@example.com>
2024-06-29 17:15:39 +0400

    commit

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts_half - func
+ * @str: variable
+ */
+void puts_half(char *str)
+{
+	int a = 0, len;
+
+	while (str[a] != '\0')
+		a++;
+
+	len = a / 2;
+
+	while (a != len)
+		_putchar(str[a]);
+
+	_putchar('\n');
+
+}
