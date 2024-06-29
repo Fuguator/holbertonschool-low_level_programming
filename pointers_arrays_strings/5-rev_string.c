c51bd8508422d839202c167ee5b605ccb66262e2
Your Name <you@example.com>
2024-06-29 17:04:19 +0400

    commit

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,7 +1,23 @@
 #include "main.h"
 /**
+ * rev_string - func
+ * @s: variable
  */
 void rev_string(char *s)
 {
+	int a, i = 0, j = 0;
+	char temp;
 
+	while (s[i] != '\0')
+		i++;
+
+	j = i - 1;
+
+	for (a = 0; a < i / 2; a++)
+	{
+		temp = s[a];
+		s[a] = s[j];
+		s[j] = temp;
+		j--;
+	}
 }
