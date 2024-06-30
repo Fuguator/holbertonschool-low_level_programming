60891328a77fb8304f1677d251f530d89eb5c4c0
Your Name <you@example.com>
2024-06-30 21:45:35 +0400

    commit

--- pointers_arrays_strings/103-infinite_add.c
+++ pointers_arrays_strings/103-infinite_add.c
@@ -1,50 +1,38 @@
 #include "main.h"
 /**
  * infinite_add - func
- * @n1: str 1
- * @n2: str 2
- * @r: result
- * @size_r: size of result
- * Return: result or 0 (if size small)
+ * @n1: variable
+ * @n2: variable
+ * @r: variable
+ * @size_r: variable
+ * Return: r
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1, len2, temp, rev, a, sum, num1, num2, carry;
-	char tmp[10000];
+	int i, j, k, l, m, n;
 
-	len1 = len2 = rev = a = sum = num1 = num2 = carry = 0;
-	for (len1 = 0; n1[len1] != '\0'; len1++)
-	for (len2 = 0; n2[len2] != '\0'; len2++)
-
-	if (len1 + 2 > size_r || len2 + 2 > size_r)
+	for (i = 0; n1[i]; i++);
+	for (j = 0; n2[j]; j++);
+	if (i > size_r || j > size_r)
 		return (0);
-
-	len1--, len2--;
-	while (a <= len1 || a <= len2)
+	m = 0;
+	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
 	{
-		num1 = num2 = 0;
-		if (a <= len1)
-			num1 = n1[len1 - a] - '0';
-		if (a <= len2 && (len2 - a) >= 0)
-			num2 = n2[len2 - a] - '0';
-		sum = num1 + num2 + carry;
-		if (sum >= 10)
-			carry = 1, sum -= 10;
-		else
-			carry = 0;
-		r[a] = sum + '0', a++, rev++;
-	}
-	if (carry > 0)
-		r[a] = carry + '0', r[a + 1] = '\0';
-	a = temp = 0;
-	while (a <= rev)
-		tmp[a] = r[rev - a], temp++, a++;
-	a = 0;
-	while (a < temp)
-	{
-		if (r[a] == '\0')
+		n = m;
+		if (i >= 0)
+			n += n1[i] - '0';
+		if (j >= 0)
+			n += n2[j] - '0';
+		if (i < 0 && j < 0 && n == 0)
+		{
 			break;
-		r[a] = tmp[a], a++;
+		}
+		m = n / 10, r[k] = n % 10 + '0';
 	}
+	r[k] = '\0';
+	if (i >= 0 || j >= 0 || m)
+		return (0);
+	for (k -= 1, l = 0; l < k; k--, l++)
+		m = r[k], r[k] = r[l], r[l] = m;
 	return (r);
 }
