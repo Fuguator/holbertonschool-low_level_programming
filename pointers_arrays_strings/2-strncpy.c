ca60655894b089abeb7c79e0163e22e22587b089
Your Name <you@example.com>
2024-06-30 10:22:41 +0400

    commit

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -11,16 +11,7 @@ char *_strncpy(char *dest, char *src, int n)
 	int a = 0;
 
 	while (dest[a] != '\0' && a < n)
-	{
-		dest[a] = src[a];
-		a++;
-	}
-
-	while (a < n)
-	{
-		dest[a] = '\0';
-		a++;
-	}
+		dest[a++] = src[a++];
 
 	return (dest);
 }
