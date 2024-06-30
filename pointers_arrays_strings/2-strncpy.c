a5b647d2dfda4f6896d00bd07f1541c45bb40e3e
Your Name <you@example.com>
2024-06-30 10:10:38 +0400

    commit

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -10,13 +10,13 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int a = 0;
 
-	while (dest[a] != '\0' && a <= n)
+	while (dest[a] != '\0' && a < n)
 	{
 		dest[a] = src[a];
 		a++;
 	}
 
-	while (a <= n)
+	while (a < n)
 	{
 		dest[a] = '\0';
 		a++;
