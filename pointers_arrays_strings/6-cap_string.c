6f044c173ad880d8ae0d6515eb9c7e48929028f6
Your Name <you@example.com>
2024-06-30 11:52:47 +0400

    commit

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * cap_string - func
+ * @str: variable
+ * Return: str
+ */
+char *cap_string(char *str)
+{
+	char sym[] = ",;.!?(){}\n\t\" ";
+	int a, i, tr = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[0] > 96 && str[0] < 123)
+			tr = 1;
+
+		for (a = 0; sym[a] != '\0'; a++)
+		{
+			if (sym[a] == str[i])
+				tr = 1;
+		}
+
+		if (tr == 1)
+		{
+			if (str[i] > 96 && str[i] < 123)
+			{
+				str[i] -= 32;
+				tr = 0;
+			}
+
+			else if (str[i] > 64 && str[i] < 91 || str[i] > 47 && str[i] < 58)
+				tr = 0;
+		}
+	}
+
+	return (str);
+}
