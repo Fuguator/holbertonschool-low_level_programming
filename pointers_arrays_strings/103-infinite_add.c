3cc96d71b50239c02029d858803a67eed4db8b76
Your Name <you@example.com>
2024-06-30 21:56:39 +0400

    commit

--- pointers_arrays_strings/103-infinite_add.c
+++ pointers_arrays_strings/103-infinite_add.c
@@ -13,8 +13,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 	for (i = 0; n1[i]; i++)
 	for (j = 0; n2[j]; j++)
-	if (i > size_r || j > size_r)
-		return (0);
+	return (i > size_r || j > size_r) ? 0 : (void)0;
 	m = 0;
 	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
 	{
