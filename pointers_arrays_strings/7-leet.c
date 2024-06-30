d49e246dc94a77ca42a101a7ab7f351bbad8f207
Your Name <you@example.com>
2024-06-30 12:04:27 +0400

    commit

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -10,19 +10,19 @@ char *leet(char *str)
 
 	while (str[a] != '\0')
 	{
-		if (str[a] == 'a' || str[a] == 'A')
+		while (str[a] == 'a' || str[a] == 'A')
 			str[a] = 4;
 
-		else if (str[a] == 'e' || str[a] == 'E')
+		while (str[a] == 'e' || str[a] == 'E')
 			str[a] = '3';
 
-		else if (str[a] == 'o' || str[a] == 'O')
+		while (str[a] == 'o' || str[a] == 'O')
 			str[a] = '0';
 
-		else if (str[a] == 't' || str[a] == 'T')
+		while (str[a] == 't' || str[a] == 'T')
 			str[a] = '7';
 
-		else if (str[a] == 'l' || str[a] == 'L')
+		while (str[a] == 'l' || str[a] == 'L')
 			str[a] = '1';
 	}
 	
