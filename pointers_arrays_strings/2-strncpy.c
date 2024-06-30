6c4e706cc34a08551cd6ab506b2abe951d1f9c21
Your Name <you@example.com>
2024-06-30 10:04:44 +0400

    commit

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strncpy - func
+ * @dest: destination
+ * @src: soource
+ * @n: size of sring
+ * Return: copied string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a = 0;
+
+	while (dest[a] != '\0' && a < n)
+		dest[a++] = src[a++];
+
+	while (a < n)
+		dest[a++] = '\0';
+
+	return (dest);
+}
