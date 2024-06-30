91b5db92f6ad41b1145fc7506a5d62507322ee82
Your Name <you@example.com>
2024-06-30 13:09:33 +0400

    commit

---
+++ pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_number - func
+ * @n: variable
+ */
+void print_number(int n)
+{
+	if (n < 0 && >= -9)
+	{
+		_putchar('-');
+		_putchar('0' - n % 10);
+	}
+	else if (n < -9)
+		print_number(n / -10);
+	else
+	{
+		if (n > 9)
+			print_number(n / 10);
+
+		_putchar(n % 10 + '0');
+	}
+}
