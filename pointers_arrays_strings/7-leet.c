56977228e940bb1df390a981af7ae8b6eee9644a
Your Name <you@example.com>
2024-06-30 12:15:01 +0400

    commit

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -7,14 +7,14 @@
 char *leet(char *str)
 {
 	char *r = str;
-	char a[] = { 'a', 'e', 'o', 't', 'l', 'A', 'E', 'O', 'T', 'L' };
-	char n[] = { 4, 3, 0, 7, 1, 4, 3, 0, 7, 1 };
+	char a[] = { 'a', 'e', 'o', 't', 'l' };
+	char n[] = { 4, 3, 0, 7, 1 };
 	int i = 0;
 
 	while (*str)
 	{
 		for (i = 0; i < 5; i++)
-			if (*str == a[i])
+			if (*str == a[i] || *str == a[i] - 32)
 				*str = n[i] + '0';
 		str++;
 	}
