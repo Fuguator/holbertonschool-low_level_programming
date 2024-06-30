db2ac54443721493b19205ea7a3c95da1a0b1a7b
Your Name <you@example.com>
2024-06-30 21:55:25 +0400

    commit

--- pointers_arrays_strings/104-print_buffer.c
+++ pointers_arrays_strings/104-print_buffer.c
@@ -12,17 +12,13 @@ void print_line(char *c, int s, int l)
 
 	for (y = 0; y <= 9; y++)
 	{
-		if (y <= s)
-			printf("%02x", c[l * 10 + y]);
-		else
-			printf("  ");
-		if (y % 2)
-			putchar(' ');
+		y <= s ? printf("%02x", c[l * 10 + y]) : printf("  ");
+		y % 2 ? putchar(' ') : 0;
 	}
 	for (z = 0; z <= s; z++)
 	{
 		if (c[l * 10 + z] > 31 && c[l * 10 + z] < 127)
-			putchar(c[l * 10 + z]);
+			putchar(c[l * 10 + z])
 		else
 			putchar('.');
 	}
@@ -39,11 +35,7 @@ void print_buffer(char *b, int size)
 	for (x = 0; x <= (size - 1) / 10 && size; x++)
 	{
 		printf("%08x: ", x * 10);
-		if (x < size / 10)
-			print_line(b, 9, x);
-		else
-			print_line(b, size % 10 - 1, x);
-
+		x < size / 10 ? print_line(b, 9, x) : print_line(b, size % 10 - 1, x);
 		putchar('\n');
 	}
 	size == 0 ? putchar('\n') : 0;
