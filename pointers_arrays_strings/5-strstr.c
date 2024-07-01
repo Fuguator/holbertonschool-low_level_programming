3b95199c34fc75de375bb583b8cb610313dbee09
Your Name <you@example.com>
2024-07-01 13:54:23 +0400

    commit

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/*
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int a = 0, b = 0, h = 0;
+
+	while (1)
+	{
+	if (needle[a] == haystack[b])
+	{
+		if (h == 0)
+			h = b;
+		a++, b++;
+		if (a >= strlen(needle))
+			return (h);
+		if (b >= strlen(haystack))
+			return (-1);
+	}
+	else
+		b++, a = 0, h = 0;
+	}
+}
