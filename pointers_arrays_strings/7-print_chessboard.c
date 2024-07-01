4e0b4c23106c4a590255d493b34ed01c92737950
Your Name <you@example.com>
2024-07-01 22:57:12 +0400

    commit

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_chessboard - func
+ * @a: variable
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i = 0, j = 0;
+
+	for (; i < 8; i++)
+		for (; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+			_putchar('\n');
+		}
+}
