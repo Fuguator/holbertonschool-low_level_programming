01f1dd3cf0f023b563e442d64170595da2aeb57d
Your Name <you@example.com>
2024-07-01 10:37:14 +0400

    commit

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * *_strchr - func
+ * @s: str
+ * @c: character
+ * Return: s + 1 if founded, NULL otherwise
+ */
+char *_strchr(char *s, char c)
+{
+	int a = 0;
+
+	while ((s[a] != c) && (s[a] != '\0'))
+		a++;
+	return ((s[a] == c) ? s + 1 : NULL);
+}
