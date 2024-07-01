755dc6b2e9c82c7ddc6c8fa89483a8ee47842563
Your Name <you@example.com>
2024-07-01 10:17:11 +0400

    commit

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * *_memcpy - func
+ * @dest: variable
+ * @src: variable
+ * @n: size
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int a;
+
+	while (a++ < n)
+		desr[a] = src[a];
+	return (dest);
+}
