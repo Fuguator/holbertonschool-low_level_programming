9d61e1184dd472b78315cd8d24f8b8dde06506d0
Your Name <you@example.com>
2024-07-01 11:10:12 +0400

    commit

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -14,7 +14,7 @@ unsigned int _strspn(char *s, char *accept)
 		for (a = 0; accept[a]; a++)
 		{
 			if (accept[a] == *s)
-				bytes++, break;
+				n++, break;
 			else if ((accept[a + 1]) == '\0')
 				return (n);
 		}
