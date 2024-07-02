3c2228f147a585141bc5739f01851272de308bff
Your Name <you@example.com>
2024-07-02 10:36:30 +0400

    commit

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * mult - func
+ * @i: variable
+ * @n: variable
+ * Return: 1 if prime
+ */
+int mult(int i, int n)
+{
+	if (i == n)
+		return (1);
+	if (n % i == 0)
+		return (0);
+	else
+		return (mult(i + 1, n);
+}
+/**
+ * is_prime_number - func
+ * @n: number
+ * Return: prime num
+ */
+int is_prime_number(int n)
+{
+	if (n > 1)
+		return (mult(2, n);
+	else
+		return (0);
+}
