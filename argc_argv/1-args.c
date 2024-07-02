974499404b1b0b3b12661cea7108b5deb11969b8
Your Name <you@example.com>
2024-07-02 10:56:46 +0000

    commit

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - func
+ * @argc: variable
+ * @argv: variable
+ * Return: 0
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
