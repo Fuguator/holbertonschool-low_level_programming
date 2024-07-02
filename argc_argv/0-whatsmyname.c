5ae34fd936432d7c0a1bdce5fbf4cac797be5649
Your Name <you@example.com>
2024-07-02 10:53:15 +0000

    commit

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -6,9 +6,8 @@
  * @argv: var
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(__attribute__((unused)) int argc, char *argv[])
 {
-	printf(argc);
 	printf("%s\n", argv[0]);
 	return (0);
 }
