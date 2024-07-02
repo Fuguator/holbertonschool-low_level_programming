124e0bacfbd4989def6f425d0bdd97f10e294ebf
Your Name <you@example.com>
2024-07-02 11:32:26 +0000

    commit

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -9,8 +9,8 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	for (i = 0; i < argc - 1; i++)
-		printf("%s ", argv[i]);
+	for (i = 0; i < argc; i++)
+		printf("%s", argv[i]);
 	printf("\n");
 	return (0);
 }
