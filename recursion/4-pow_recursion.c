f3e2aa5d41f2b20626567f78fe5e05df825ba50f
Your Name <you@example.com>
2024-07-02 10:19:35 +0400

    commit

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -7,7 +7,12 @@
  */
 int _pow_recursion(int x, int y)
 {
-	return (y == 0 ? 1 : NULL);
-	return (y == 1 ? x : NULL);
-	return (y > 1 ? x * _pow_recursion(x, y - 1) : -1);
+	if (y == 0)
+		return (1);
+	if (y == 1)
+		return (x);
+	if (y > 1)
+		return (x * _pow_recursion(x, y - 1));
+	else
+		return (-1);
 }
