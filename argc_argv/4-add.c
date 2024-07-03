a0983302a77d8a4945e9dec070b2433d1f8d52bd
Your Name <you@example.com>
2024-07-03 12:57:51 +0400

    commit

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -8,7 +8,7 @@
  * @argv: var
  * Return: 1 if error, 0 otherwise
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv)
 {
 	int i;
 	unsigned int sum = 0, x;
@@ -21,7 +21,7 @@ int main(int argc, char **argv)
 			a = argv[i];
 			for (x = 0; x < strlen(a); x++)
 			{
-				if ((a[x] < 48) || (a[x] > 57))
+				if ((a[x] < 48) && (a[x] > 57))
 				{
 					printf("Error\n");
 					return (1);
