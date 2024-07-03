5b211757d3c7f605fac275e740ae4e6d0f6d70ba
Your Name <you@example.com>
2024-07-03 11:22:49 +0400

    commit

---
+++ argc_argv/4-add.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ */
+int main(int argc, char *argv)
+{
+	int i, sum = 0;
+
+	while (i < argc - 1)
+	{
+		if ((argv[i] >= 'a' && argv[i] >= 'z') || (argv[i] >= 'A' && argv[i] >= 'Z'))
+		{
+			printf("Error\n");
+			return (0);
+		}
+		else
+		{
+			
+			sum += atoi(argv[i]);
+			printf("%d\n", sum);
+		}
+	return (0);
+}
