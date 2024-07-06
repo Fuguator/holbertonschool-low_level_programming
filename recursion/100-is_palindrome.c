c5b1e6eff77b4f090c70531af7601e8120f1c2b8
Your Name <you@example.com>
2024-07-06 10:30:41 +0400

    commit

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -5,14 +5,19 @@ int is_palindrome(char *s)
 {
 	int i, a;
 
-	while (*s != '\n')
-		a++;
-
-	for (i = 0; i < a; i++)
+	if (sizeof(s) > 4)
 	{
-		if (s[i] == s[a - i])
-			return (1);
-		else
-			return (0);
+		while (*s != '\n')
+			a++;
+
+		for (i = 0; i < a; i++)
+		{
+			if (s[i] == s[a - i])
+				return (1);
+			else
+				return (0);
+		}
 	}
+	else
+		return (1);
 }
