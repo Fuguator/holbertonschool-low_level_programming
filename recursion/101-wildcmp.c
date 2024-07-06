15a8bacf3d3ca87bfd0b538bca016815a69db13e
Your Name <you@example.com>
2024-07-06 19:02:40 +0400

    commit

--- recursion/101-wildcmp.c
+++ recursion/101-wildcmp.c
@@ -9,5 +9,5 @@ int wildcmp(char *s1, char *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 	if (*s2 == '*')
 		return wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2));
-	return (0);
+	return (1);
 }
