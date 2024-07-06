5c3f84e4549c45c23a3178b5bffd3494ee5fb385
Your Name <you@example.com>
2024-07-06 18:53:08 +0400

    commit

---
+++ recursion/101-wildcmp.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (s1 == '\n' && s2 == '\n')
+		return (1);
+	if (s1 == s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (s2 == '*')
+		return wildcmp(s1, s2 + 1) || (s1 != '\0' && wildcmp(s1 + 1, s2));
+}
