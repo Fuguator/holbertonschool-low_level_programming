06b8d046357e4837a9be327bcbe9a755580f1c97
Your Name <you@example.com>
2024-07-07 16:10:40 +0400

    commit

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -8,32 +8,34 @@
  */
 int main(int argc, char *argv[])
 {
-        int cents, money;
-
-        if (argc != 2)
-        {
-                printf("Error\n");
-                return (1);
-        }
-
-        money = atoi(argv[1]);
-                                                                              if (money <= 0)
-        {
-                printf("0\n");
-                return (0);
-        }
-
-        for (cents = 0; money > 0; cents++)
-        {
-                if (money >= 25)                                                              money -= 25;
-                else if (money < 25 && money >= 10)
-                        money -= 10;
-                else if (money < 10 && money >= 5)
-                        money -= 5;
-                else if (money < 5 && money >= 2)
-                        money -= 2;
-                else
-                        money -= 1;
+	int cents, money;
+	
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	
+	money = atoi(argv[1]);
+	
+	if (money <= 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	
+	for (cents = 0; money > 0; cents++)
+	{
+		if (money >= 25)
+			money -= 25;
+		else if (money < 25 && money >= 10)
+			money -= 10;
+		else if (money < 10 && money >= 5)
+			money -= 5;
+		else if (money < 5 && money >= 2)
+			money -= 2;
+		else if (money == 1)
+			money -= 1;
 	}
 
 	printf("%d\n", cents);
