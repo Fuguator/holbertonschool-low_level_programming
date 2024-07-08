3a3ae520a43e14705a4ab4a3c25f01522d204050
Your Name <you@example.com>
2024-07-08 15:18:43 +0400

    commit

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -2,14 +2,25 @@
 #include <stddef.h>
 #include <stdlib.h>
 /**
+ * *create_array - func
+ * @size: var
+ * @c: var
+ * Return: a if str not empty
  */
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i = 0;
+	char *a
+
+	*a = malloc(sizeof(size) * a);
+
+	if (size == 0 || a == NULL)
+		return (NULL);
 
 	while (i < size)
 	{
-		c[i] = '*';
+		a[i] = c;
 		i++;
 	}
+	return (a);
 }
