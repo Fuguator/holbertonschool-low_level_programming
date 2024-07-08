5a29039dc42ad47cb787a0bda0ecd3e3fd1c0ea8
Your Name <you@example.com>
2024-07-08 15:20:55 +0400

    commit

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -12,7 +12,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 	char *a;
 
-	*a = malloc(sizeof(size) * a);
+	*a = malloc(sizeof(a) * size);
 
 	if (size == 0 || a == NULL)
 		return (NULL);
