96d0bbd64b88e488a20a6abe9ac5b72da6671140
Your Name <you@example.com>
2024-07-10 14:33:01 +0400

    commit

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -12,7 +12,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	res = malloc(height * sizeof(int));
+	res = malloc(height * sizeof(int *));
 
 	if (res == NULL)
 	{
