f63e8c9dfea25588baeb45c853710bda237f8bc2
Your Name <you@example.com>
2024-07-10 14:44:06 +0400

    commit

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -3,15 +3,12 @@
  * free_grid - func
  * @grid: matrix
  * @height: var
- * Return: NULL
  */
 void free_grid(int **grid, int height)
 {
 	int i;
 
-	if (grid == NULL)
-		return (NULL);
-	else
+	if (grid != NULL)
 	{
 		for (i = 0; i < height; i++)
 			free(grid[i]);
