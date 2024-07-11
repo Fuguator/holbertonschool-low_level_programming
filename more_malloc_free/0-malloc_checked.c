2e2eccd45379256197b22946722f2505c130d3d7
Your Name <you@example.com>
2024-07-11 10:16:58 +0400

    commit

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * malloc_checked - func
+ * @b: var
+ * Return: p
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *p
+
+	p = malloc(b);
+
+	if (p == NULL)
+		exit(98);
+	return (p);
+}
---
+++ more_malloc_free/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+void *malloc_checked(unsigned int b);
+
+#endif
