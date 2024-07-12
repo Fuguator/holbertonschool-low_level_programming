f97bb93f57094039ea9ca55346f84b0c05afaf3f
Your Name <you@example.com>
2024-07-12 11:51:38 +0400

    commit

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,9 +1,14 @@
 #include "main.h"
 /**
+ * string_nconcat - func
+ * @s1: str1
+ * @s2: str2
+ * @n: var
+ * Return: strncat
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i = 0, j = 0, res;
+	int i = 0, j = 0, k = 0;
 	char *a;
 
 	if (s1 != NULL)
@@ -22,5 +27,20 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (a == NULL)
 		return (NULL);
 
+	while (s1 != '\0')
+	{
+		a[i] = s1[i];
+		i++;
+	}
+	k = i;
+	i = 0;
 
+	while (s2 != '\0')
+	{
+		a[k + i] = s2[i];
+		i++;
+	}
+	str[i + k] = '\0';
+
+	return (a);
 }
