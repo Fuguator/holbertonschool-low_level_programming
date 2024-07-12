216db946369087f6ccbbc829fc0541719a594f69
Your Name <you@example.com>
2024-07-12 14:32:43 +0400

    commit

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i = 0;
+	char *a;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	a = malloc(sizeof(nmemb) * size);
+
+	while (a)
+		a[i] = 0;
+
+	return (a);
+
+}
