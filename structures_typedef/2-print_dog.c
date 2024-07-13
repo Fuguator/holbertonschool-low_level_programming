21a30db7436d22d044f664ffcfb95d806eea3103
Your Name <you@example.com>
2024-07-13 16:36:07 +0400

    commit

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -6,14 +6,19 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
+		printf("Name: ");
 		if ((*d).name != NULL)
 			printf("%s\n", (*d).name);
 		else
 			printf("(nil)\n");
+
+		printf("Age: ");
 		if (!(*d).age)
 			printf("%f\n", 0.);
 		else
 			printf("%f\n", (*d).age);
+
+		printf("Owner: ");
 		if ((*d).owner != NULL)
 			printf("%s\n", (*d).owner);
 		else
