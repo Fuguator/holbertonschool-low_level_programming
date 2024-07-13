6c3747a50b2c72fc52a81c70477aa3b91a723cef
Your Name <you@example.com>
2024-07-13 16:48:44 +0400

    commit

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,40 @@
+#include "dog.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * new_dog - func
+ * @name: var
+ * @age: var
+ * @owner: var
+ * Return: dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog = malloc(sizeof(dog_t));
+
+	if (dog == NULL)
+		return (NULL);
+
+	(*dog).name = malloc(strlen(name) + 1);
+
+	if ((*dog).name == NULL)
+	{
+		free((*dog).name), free(dog);
+		return (NULL);
+	}
+
+	(*dog).age = age;
+
+	(*dog).owner = malloc(strlen(owner) + 1);
+
+	if ((*dog).owner == NULL)
+	{
+		free((*dog).owner), free((*dog).name), free(dog);
+		return (NULL);
+	}
+
+	strcpy((*dog).name, name), strcpy((*dog).owner, owner);
+
+	return (dog);
+}
