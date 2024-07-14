237a438f04b1286d328ea2dfa308fceff98a7bbb
Your Name <you@example.com>
2024-07-14 17:34:05 +0400

    commit

---
+++ malloc_free/100-argstostr.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * argstostr - func
+ * @ac: var
+ * @av: var
+ * Return: a
+ */
+char *argstostr(int ac, char **av)
+{
+	int i = 0, j = 0, k = 0;
+	char *a;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (; i < ac; i++)
+		for (; av[i][j]; j++)
+			k++;
+
+	a = malloc(sizeof(char) * (1 + k + ac));
+	i = j = k = 0;
+
+	if (a == NULL)
+		return (NULL);
+
+	for (; i < ac; i++)
+	{
+		for (; av[i][j]; j++)
+		{
+			a[k] = av[i][j];
+			k++;
+		}
+		a[k] = '\n';
+		i++, k++;
+	}
+	a[k] = '\0';
+
+	return (a);
+}
