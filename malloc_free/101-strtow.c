314606c1bddb729e8e164767655603023c383576
Your Name <you@example.com>
2024-07-14 23:38:16 +0400

    commit

---
+++ malloc_free/101-strtow.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * strtow - var
+ * @str: var
+ * Return: NULL
+ */
+char **strtow(char *str)
+{
+	char *a = NULL;
+	unsigned int i = 0, j = 0, k;
+
+	if (strncmp(str, "", 1) || str == NULL)
+		return (NULL);
+	a = malloc((i + j + 1) * sizeof(char));
+	if (a == NULL)
+		return (NULL);
+	for (k = 0; k < i; k++)
+		a[k] = str[k];
+	i = k;
+	for (k = 0; k < j; k++)
+	{
+		a[i] = str[k];
+		i++;
+	}
+	a[i] = '\0';
+	return (NULL);
+}
