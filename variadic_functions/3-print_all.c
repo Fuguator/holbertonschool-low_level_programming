d565c7b5147f874b212b2ba7c438d8582054d8d9
Your Name <you@example.com>
2024-07-16 16:10:41 +0400

    commit

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -32,9 +32,7 @@ void print_all(const char * const format, ...)
                                 break;
 			case 's':
 				a = va_arg(ap, char *);
-				if (a)
-					printf("%s", a);
-				else
+				if (!a)
 					a = "(nil)";
 				break;
 			}
