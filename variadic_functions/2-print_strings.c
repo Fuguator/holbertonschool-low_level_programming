b13ccdff54fa288991bbdcd7ab71a866700fecce
Your Name <you@example.com>
2024-07-16 15:07:39 +0400

    commit

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -5,6 +5,7 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	int i = 0;
+	char *a;
 	va_list ap;
 
 	if (n == 0)
@@ -14,9 +15,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (; i < (int)n; i++)
 	{
-		printf("%s", va_arg(ap, char));
-		if (i < (int)n - 1 && separator)
-			printf("%s", separator);
+		s = va_arg(ap, char *);
+
+		s == NULL ? printf("(nil)") : printf("%s, s");
+		(i < (int)n - 1 && separator) ? printf("%s", separator) : 0; 
 	}
 	printf("\n");
 	va_end(ap);
