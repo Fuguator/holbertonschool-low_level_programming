f4c95af1beb73de1e1473388525fa2ec4e94a078
Your Name <you@example.com>
2024-07-16 09:22:51 +0400

    commit

---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,8 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+int _putchar(char c);
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+void print_name(char *name, void (*f)(char *));
+#endif
