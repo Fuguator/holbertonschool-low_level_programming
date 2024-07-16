7583412c54a51c600c24ba2d2422de1b08f2dadb
Your Name <you@example.com>
2024-07-16 10:37:28 +0400

    commit

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * sum_them_all - func
+ * @n: var
+ * Return: sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int i = 0, sum = n;
+	va_list ap;
+
+	if (n == 0)
+		return (0);
+
+	va_start(ap, n);
+
+	for (; i < n(int); i++)
+		sum += va_arg(ap, int);
+
+	va_end(ap);
+	return (sum);
+}
---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+int _putchar(char c);
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+int sum_them_all(const unsigned int n, ...);
+
+#endif
