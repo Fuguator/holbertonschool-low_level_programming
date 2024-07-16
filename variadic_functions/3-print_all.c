f34c1784c48a163eb18843020278257787aa7e87
Your Name <you@example.com>
2024-07-16 15:37:02 +0400

    commit

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,41 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * print_all - func
+ * @format: int/char/float/string
+ */
+void print_all(const char * const format, ...)
+{
+	int i = 0, j = 0;
+	char *a;
+	va_list ap;
+
+	if (format)
+	{
+		for (; *(format + i); i++)
+		{
+			k = 1;
+
+			switch (*(format + i--))
+			case 'c':
+				printf("%c", va_arg(ap, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(ap, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(ap, int));
+				break;
+			case 's':
+				a = va_arg(ap, char *);
+				a != NULL ? printf("%s", a) : printf("(nil)");
+				break;
+		}
+		while (*(format + i) && j)
+			printf(", ");
+
+		j = 0;
+	}
+	printf("\n");
+	va_end(ap);
+}
