05668541dfae674bf12818e916f80f1a317f1918
Your Name <you@example.com>
2024-07-16 10:13:04 +0400

    commit

---
+++ function_pointers/3-calc.h
@@ -0,0 +1,11 @@
+#ifndef CALC.H
+#define CALC.H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+#endif
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,41 @@
+#include "calc.h"
+/**
+ * op_add - func
+ * @a: var
+ * @b: var
+ * Return: res
+ */
+int op_add(int a, int b)
+	return (a + b);
+/**
+ * op_sub - func
+ * @a: var
+ * @b: var
+ * Return: res
+ */
+int op_sub(int a, int b)
+	return (a - b);
+/**
+ * op_mul - func
+ * @a: var
+ * @b: var
+ * Return: res
+ */
+int op_mul(int a, int b)
+	return (a * b);
+/**
+ * op_div - func
+ * @a: var
+ * @b: var
+ * rETURN: RES
+ */
+int op_div(int a, int b)
+	return (a / b);
+/**
+ * op_mod - func
+ * @a: var
+ * @b: var
+ * Return: res
+ */
+int op_mod(int a, int b)
+	return (a % b);
