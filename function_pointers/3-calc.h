3db1939404ffd8f7ea1615763bcee80ea874b3ac
Your Name <you@example.com>
2024-07-18 11:48:55 +0400

    commit

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -4,6 +4,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
