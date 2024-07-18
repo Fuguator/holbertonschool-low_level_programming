c0c514eaad099cf5d4440d4d7b6559c112834088
Your Name <you@example.com>
2024-07-18 11:47:38 +0400

    commit

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -3,24 +3,24 @@
  * get_op_func - func
  * @s: var
  * Return: NULL
- */
+*/
 int (*get_op_func(char *s))(int, int)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
 
-    while (i < sizeof(ops) / sizeof(opt_t))
-    {
-	    if (*s == *ops[i].s)
-		    return (ops[i].f);
-	    i++;
-    }
-    return (NULL);
+	while (i < 5)
+	{
+		if (ops[i].op[0] == s[0])
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
 }
