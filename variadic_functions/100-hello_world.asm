4f99c40f56a0c93b4e4c94bd13ef6bf263275c37
Your Name <you@example.com>
2024-07-20 21:05:34 +0400

    commit

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -7,12 +7,12 @@ global main
 section .text
 
 main:
-	mov eax, 0x1
-	mov ebx, 0x1
-	mov ecx, message
-	mov edx, message_length
+	mov rax, 0x1
+	mov rdi, 0x1
+	mov rsi, message
+	mov rdx, message_length
 	SYSCALL
 
-	mov eax, 0x3C
-	mov ebx, 0x0
+	mov rax, 0x3C
+	mov rdi, 0x0
 	SYSCALL
