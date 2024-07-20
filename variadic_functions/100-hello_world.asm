65c3491ad9a22421f11fc2c2af28da9344396f05
Your Name <you@example.com>
2024-07-20 20:58:51 +0400

    commit

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -2,19 +2,17 @@ section .data:
         message: db "Hello, World", 0xA
         message_length equ $-message
 
-global _start
+global main
 
 section .text
 
-_start:
+main:
 	mov eax, 0x4
 	mov ebx, 1
 	mov ecx, message
 	mov edx, message_length
-	int 0x80
 	SYSCALL
 
 	mov eax, 0x1
 	mov ebx, 0
-	int 0x80
 	SYSCALL
