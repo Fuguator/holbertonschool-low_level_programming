26458c23320b79faa777476afa8585ba6400010c
Your Name <you@example.com>
2024-07-31 13:43:54 +0400

    3-add_node_end.c was debugged

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,41 +1,49 @@
+
 #include "lists.h"
 /**
- * add_node_end - adds a new node at the end
- * @head: adress
- * @str: data
- * Return: last element
+ * add_node_end - function
+ * @head: double pointer to the head of linked list
+ * @str: pointer to new string in node
+ * Return: adress of new element
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-  int i = 0, len = 0;
-  list_t *node_end, *tmp;
+	int i = 0, len = 0;
+	list_t *node_end, *tmp;
 
-  if (!str)
-    return (NULL);
+	if (!str)
+		return (NULL);
 
-  while(*(str + i))
-    len++, i++;
-  i = 0;
+	while (*(str + len))
+		len++;
 
-  node_end = malloc(sizeof(list_t));
+	node_end = malloc(sizeof(list_t));
 
-  if (!node_end)
-    return (NULL);
+	if (!node_end)
+		return (NULL);
 
-  node_end->str = strdup(str);
-  node_end->len = len;
-  node_end->next = NULL;
+	node_end->str = strdup(str);
+	if (!node_end->str)
+	{
+		free(node_end);
+		return (NULL);
+	}
 
-  if (*head == NULL)
-  {
-    *head = node_end;
-    return (node_end);
-  }
-  tmp = *head;
-  while (tmp->next)
-  {
-    tmp = tmp->next;
-    tmp->next = node_end;
-  }
-  return (node_end);
+	node_end->len = len;
+	node_end->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = node_end;
+		return (node_end);
+	}
+
+	tmp = *head;
+
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = node_end;
+
+	return (node_end);
 }
+
