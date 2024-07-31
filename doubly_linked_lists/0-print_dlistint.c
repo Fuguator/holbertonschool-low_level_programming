44199ae2dc777c0f6f6338f78cb55d81b258efad
Your Name <you@example.com>
2024-07-31 17:40:41 +0400

    commit

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -6,12 +6,12 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	int i = 0;
 	const dlistint_t *a = h;
+	int i = 0;
 
 	for (; h; i++)
 	{
-		printf("%d", a->n);
+		printf("%d", a->h);
 		a = a->next;
 	}
 	return (i);
