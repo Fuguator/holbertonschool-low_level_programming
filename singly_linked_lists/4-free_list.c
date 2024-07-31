677dc5cd68232914a8468589be034d2bc332a467
Your Name <you@example.com>
2024-07-31 16:06:48 +0400

    4-free_list.c added

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_list - func
+ * @head: adress
+ */
+void free_list(list_t *head)
+{
+	list_t *a;
+	
+	while (head)
+	{
+		a = head->next;
+		free(head->str);
+		free(head);
+		head = a;
+	}
+}
