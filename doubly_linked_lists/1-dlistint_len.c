4860051eceba89e3784da6563922148cd21e1c66
Your Name <you@example.com>
2024-07-31 18:07:10 +0400

    commit

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,11 +1,14 @@
 #include "lists.h"
 /**
+ * dlistint_len - func
+ * @h: var
+ * Return: i
  */
 size_t dlistint_len(const dlistint_t *h)
 {
 	int i = 0;
-	const dlistint_t *a = h;
 
 	for (; a; i++)
-		a = a->next;
+		h = h->next;
 	return (i);
+}
