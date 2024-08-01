5987c65d06fb8bc789447c67902369231b8efdf3
Your Name <you@example.com>
2024-08-01 16:30:39 +0400

    3-add_dnodeint_end.c added, readme && lists.h are updated

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+ * add_dnodeint_end - adds a node to the end of list
+ * @head: adress
+ * @n: data
+ * Return: new_node
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *tmp = *head, *new_node = malloc(sizeof(dlistint_t));
+
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (!(*head))
+	{
+		new_node->prev = NULL;
+		*head = new_node;
+		return (NULL);
+	}
+
+	while (tmp->next)
+		tmp = tmp->next;
+
+	tmp->next = new_node;
+	new_node->prev = tmp;
+
+	return (new_node);
--- doubly_linked_lists/README.md
+++ doubly_linked_lists/README.md
@@ -1,1 +1,1 @@
-not empty readme
+All the tasks are written in VS Code or Replit, that's why there are so few commits.
