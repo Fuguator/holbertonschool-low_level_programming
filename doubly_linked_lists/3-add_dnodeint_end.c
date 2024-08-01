ce4117f3b1eacb552fb9f95483f30ffb5de25844
Your Name <you@example.com>
2024-08-01 16:35:27 +0400

    debugged

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -9,25 +9,24 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *tmp = *head, *new_node = malloc(sizeof(dlistint_t));
 
-	if (!new_node)
+	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = n;
 	new_node->next = NULL;
 
-	if (!(*head))
+	if (*head == NULL)
 	{
 		new_node->prev = NULL;
 		*head = new_node;
 		return (NULL);
 	}
 
-	while (tmp->next)
-	{
+	while (tmp->next != NULL)
 		tmp = tmp->next;
-		tmp->next = new_node;
-		new_node->prev = tmp;
-	}
+
+	tmp->next = new_node;
+	new_node->prev = tmp;
 
 	return (new_node);
 }
