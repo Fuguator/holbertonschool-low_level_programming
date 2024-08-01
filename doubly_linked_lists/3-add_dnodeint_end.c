2f604de63ede481a0f6637db1705cc27c5fb1077
Your Name <you@example.com>
2024-08-01 16:33:20 +0400

    debugged

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -23,10 +23,11 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	}
 
 	while (tmp->next)
+	{
 		tmp = tmp->next;
-
-	tmp->next = new_node;
-	new_node->prev = tmp;
+		tmp->next = new_node;
+		new_node->prev = tmp;
+	}
 
 	return (new_node);
 }
