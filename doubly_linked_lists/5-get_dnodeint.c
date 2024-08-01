a6baab818451c33a4ecc6031b4a12dc515ba790f
Your Name <you@example.com>
2024-08-01 19:00:10 +0400

    5-get_dnodeint.c added

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,23 @@
+#inlude "lists.h"
+/**
+ * get_dnodeint_at_index - func
+ * @head: adress
+ * @index: value
+ * Return: nth node of a list
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *a = head;
+	unsigned int i = 0;
+
+	if (!head)
+		return (NULL);
+
+	for (; a; i++)
+	{
+		if (i == index)
+			return (i);
+		a = a->next;
+	}
+	return (a);
+}
