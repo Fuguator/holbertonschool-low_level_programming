ead77ec715e6a3fe79e339a8c94d73ee213e637c
Your Name <you@example.com>
2024-08-03 11:59:51 +0400

    debugged

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -5,7 +5,7 @@
  * @index: index
  * Return: -1
  */
-int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *tmp = *head, *del_node;
 	unsigned int i = 0;
