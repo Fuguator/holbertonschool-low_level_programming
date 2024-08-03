6542e0b66844bd28119aba5b61fd260ac3acfcca
Your Name <you@example.com>
2024-08-03 19:57:39 +0400

    debugged

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 		return (1);
 	}
 	else
-		while (text_content)
+		while (*(text_content + i))
 			i++;
 
 	w = write(fd, text_content, i);
