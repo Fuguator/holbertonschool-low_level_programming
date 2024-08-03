31b76469ed73d3c2bf81be9962e3462df50351e0
Your Name <you@example.com>
2024-08-03 15:55:16 +0400

    4-clear_bit.c added

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * clear_bit - func
+ * @n: num
+ * @index: index
+ * Return: -1 if error, 1 otherwise
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+	else
+	{
+		n &= (~(1 << index));
+		return (1);
+	}
+}
