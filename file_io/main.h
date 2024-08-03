7d442cede29cafc3d4b4122f85274e57d2c1c190
Your Name <you@example.com>
2024-08-03 19:34:19 +0400

    debugged

--- file_io/main.h
+++ file_io/main.h
@@ -3,6 +3,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stddef.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 #endif
