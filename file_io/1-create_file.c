115ed23a9488313911835ea53743f728793ce9da
Your Name <you@example.com>
2024-08-03 19:53:56 +0400

    1-create_file.c added

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, 0_CREAT | 0_RDWR | 0_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
