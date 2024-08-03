e394e1b8a9e259e03699a697dbb4fbf08d4e635a
Your Name <you@example.com>
2024-08-03 15:28:59 +0400

    3-set_bit.c added

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * set_bit - func
+ * @n: num
+ * @index: index
+ * Return: -1 if error, 1 ootherwise
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+	else
+	{
+		*n |= (1 << index);
+		return (1);
+	}
+}
