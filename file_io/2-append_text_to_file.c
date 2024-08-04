e7a9a61a59d313ee51bc80ddbfccfe5f50006622
Your Name <you@example.com>
2024-08-04 12:43:34 +0400

    debugged

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -12,7 +12,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 
 	if (fd == -1)
 		return (-1);
@@ -25,7 +25,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	else
 		w = write(fd, text_content, i);
 
-	if (w == -1 || w != i)
+	if (w == -1)
 		return (-1);
 
 	close(fd);
